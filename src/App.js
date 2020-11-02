cc3c81a64ff0b8f1baeed88b3f7b1c66fde28df2
Imani Paul <imani.paul1@gmail.com>
2020-11-01 23:00:35 -0500

    Setup default particles background

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import Particles from "react-tsparticles";
+
+import particlesOptions from "./particles.json";
 
 function App() {
-  return <div className="App">Portfolio Update</div>;
+  return (
+    <div className="App">
+      Portfolio Update
+      <Particles options={particlesOptions} />
+    </div>
+  );
 }
 
 export default App;
