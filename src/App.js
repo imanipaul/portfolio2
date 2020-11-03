ad3b70341310f5e9c875c59a2c9b2e724b93797b
Imani Paul <imani.paul1@gmail.com>
2020-11-02 20:26:56 -0500

    Display particles in background

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ import particlesOptions from "./particles.json";
 function App() {
   return (
     <div className="App">
-      Portfolio Update
+      <h1>Portfolio Update</h1>
+      <p>
+        This is some information about my portfolio. Click here to do something
+        important. Idk, we'll see what happens
+      </p>
       <Particles options={particlesOptions} />
     </div>
   );
