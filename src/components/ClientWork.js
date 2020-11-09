5645b5dbf990da67320ade1dd3a77b3cfdbd83dd
Imani Paul <imani.paul1@gmail.com>
2020-11-09 11:25:38 -0500

    Additional client projects

--- src/components/ClientWork.js
+++ src/components/ClientWork.js
@@ -1,6 +1,8 @@
 import React from "react";
 import att from "../assets/forces-of-change.png";
 import hulu from "../assets/shape-the-future.png";
+import hsbc from "../assets/hsbc.png";
+import jh from "../assets/jh.png";
 import "../styles/ClientWork.scss";
 import Tag from "./Tag";
 
@@ -39,15 +41,32 @@ export default function ClientWork() {
           </div>
         </div>
         <div className="project">
-          <img src={hulu} alt="preview" />
+          <img src={hsbc} alt="preview" />
           <div className="info">
-            <p className="name">HSBC Fleece Stuff</p>
+            <p className="name">HSBC Articles</p>
             <p className="description">
-              Responsive landing page for AT&T Forces of Change video series.
+              Responsive hub for Investment Grade Cashmere and Securing the
+              World articles, as well as a whitepaper.
             </p>
             <div className="tags">
               <Tag title={"React.js"} />
               <Tag title={"Sass"} />
+              <Tag title={"Parallax Design"} />
+            </div>
+          </div>
+        </div>
+        <div className="project">
+          <img src={jh} alt="preview" />
+          <div className="info">
+            <p className="name">John Hancock Hub</p>
+            <p className="description">
+              Responsive hub built for John Hancock includes several article
+              pages, video pages, and an infographic.
+            </p>
+            <div className="tags">
+              <Tag title={"React.js"} />
+              <Tag title={"Sass"} />
+              <Tag title={"Youtube API"} />
             </div>
           </div>
         </div>
