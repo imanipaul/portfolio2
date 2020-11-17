19744574ff3685fb12f5cd6a3343d97d2f6046f7
Imani Paul <imani.paul1@gmail.com>
2020-11-16 20:50:29 -0500

    Toggle hidden state on button click

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../assets/Logo.png";
 import "../styles/Nav.scss";
 
 export default function Nav() {
+  const [hidden, setHidden] = useState(true);
+
   return (
     <nav>
-      <div>
+      <button onClick={() => setHidden(!hidden)}>Open</button>
+      <div className={hidden ? "hidden" : "visible"}>
         <p>About</p>
         <p>Work</p>
         <p>Projects</p>
