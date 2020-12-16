d4ef756dc00c4515326f010d6f878905d950bc1e
Imani Paul <imani.paul1@gmail.com>
2020-12-16 16:35:16 -0500

    Add href to footer icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,15 +9,34 @@ export default function Footer() {
   return (
     <footer>
       <div className="icons">
-        <div className="icon">
-          <AiOutlineLinkedin size={"1.5em"} />
-        </div>
-        <div className="icon">
-          <FiGithub size={"1.5em"} />
-        </div>
-        <div className="icon">
-          <AiOutlineMail size={"1.5em"} />
-        </div>
+        <a
+          href="https://www.linkedin.com/in/imanipaul"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="icon">
+            <AiOutlineLinkedin size={"1.5em"} />
+          </div>
+        </a>
+        <a
+          href="https://www.github.com/imanipaul"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="icon">
+            <FiGithub size={"1.5em"} />
+          </div>
+        </a>
+
+        <a
+          href="mailto:imanipaul1@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="icon">
+            <AiOutlineMail size={"1.5em"} />
+          </div>
+        </a>
       </div>
 
       <div className="disclaimer">
